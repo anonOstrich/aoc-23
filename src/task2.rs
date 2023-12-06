fd4f6754eb175c4fe3c8e24ac3a2b26c9b74114e
Jesper Kuutti <jesper.kuutti@gmail.com>
2023-12-06 16:19:47 +0200

    Solved day 6

--- src/task2.rs
+++ src/task2.rs
@@ -1,201 +1,47 @@
-use core::{fmt, panic};
-use std::{str::{FromStr, Lines}, fmt::{Error}};
 
+// For a given T = time_limit we can express the result as a function of charging  time x (0..=T):
+// movement(t) = 0*x + (T-x) * x.
+// Then we're looking for interger solutions to the inequality movement(t) > T
+// by the power of Math we observe that the possible solutions are found between the two zero points of the parabola
+// so we just need to solve the equality movement(t) - T = 0 and we know how many solutions there are
+fn extreme_solutions(time_limit: &i64, record: &i64) -> (f64, f64) {
+    let a = -1 as f64;
+    let b = *time_limit as f64;
+    let c = -1.0 *  (*record as f64);
 
-#[cfg(windows)]
-static LINE_ENDING: &str = "\r\n";
-#[cfg(not(windows))]
-static LINE_ENDING: &str = "\n";
+    let mut sol1 = -b + f64::sqrt(b*b - 4.0 * a* c);
+    sol1 /= 2.0 * a;
 
+    let mut sol2 = -b - f64::sqrt(b*b - 4.0 * a* c);
+    sol2 /= 2.0 * a;
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-enum Range {
-    Range(i64, i64),
-    Empty
+    return (sol1, sol2);
 }
 
-impl Range {
-    fn intersection(&self, other: &Self) -> Self {
-        let something = match (self, other) {
-            (Range::Empty, _) => Range::Empty,
-            (_, Range::Empty) => Range::Empty,
-            (Range::Range(s1, w1),Range::Range(s2, w2)) => {
-                let e1 = s1 + w1;
-                let e2 = s2 + w2;
+fn  is_integer(x: &f64) -> bool {
+    x.fract() == 0.00
+}
 
-                let start = std::cmp::max(s1, s2);
-                let end = std::cmp::min(e1, e2);
-                if *start >= end {
-                    return Range::Empty;
-                }
-                return Range::Range(*start, end - start);
-            }
-        };
-        return something;
+fn count_solutions((min, max): (f64, f64)) -> i64 {
+    let mut narrowing = 0;
+    if is_integer(&min) {
+        narrowing += 1;
     }
-
-    fn subtract(&self, other: &Self) -> Vec<Self> {
-        let inter = self.intersection(other);
-        if inter == Self::Empty {
-            return vec![*self];
-        }
-        if *self == Self::Empty {
-            return vec![];
-        }
-
-        let Self::Range (s1, w1)= *self else {panic!()};
-        let Self::Range(s2, w2) = inter else {panic!()};
-
-        if s2 == s1 {
-            if w1 == w2 {
-                return vec![Self::Empty];
-            }
-            return vec![Self::Range(s2 + w2, w1 - w2)];
-        }
-
-        if s1 + w1 == s2 + w2 {
-            return vec![Self::Range(s1, s2 - s1)]
-        }
-
-
-        // need to create two elements for the result...
-        return vec![
-            Self::Range(s1,s2 - s1),
-            Self::Range(s2 + w2, s1 + w1 - (s2 + w2))
-        ];
+    if is_integer(&max) {
+        narrowing += 1;
     }
-
-    
+    let res = (max.floor() + 1.00) - min.ceil();
+    return res as i64 - narrowing;
 }
-
-struct Mapping {
-    source: i64,
-    target: i64,
-    range: i64
-}
-
-impl Mapping {
-
-    fn map(&self, input: &Range) -> (Range, Range) {
-        // these would make much more sense as properties of mapping
-        let source_range = Range::Range(self.source, self.range);
-        let target_range = Range::Range(self.target, self.range);
-
-        let source_intersection = input.intersection(&source_range);
-        if  Range::Empty == source_intersection {
-            return (Range::Empty, Range::Empty);
-        }
-        let Range::Range(x, y ) = source_intersection else {panic!("Oh no")};
-        let target_unbounded = Range::Range(self.target + (x - self.source), y);
-        let target = target_unbounded.intersection(&target_range);
-        return (target, source_intersection);
-    }
-}
-
-impl FromStr for Mapping {
-    type Err = Error;
-
-    // Expects a string of type "\d \d \d", fails otherwise
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let numbers: Vec<i64> = s.split(' ')
-        .map(|x| x.parse().expect("Failed to parse mapping data as number")).collect();
-        if numbers.len() != 3 {
-            //...don't really understand error types well yet
-            return Err(fmt::Error);
-        }
-
-        Ok(Mapping {
-            target: numbers[0],
-            source: numbers[1],
-            range: numbers[2]
-        })
-    }
-}
-
-
-
-struct Converter {
-    mappings: Vec<Mapping>
-}
-
-impl Converter {
-    fn from(lines: &str) -> Self {
-        let mut mappings: Vec<Mapping> = lines.lines()
-            .map(|line| line.parse().expect("Failed to parse a number line"))
-            .collect();
-
-        Converter{ mappings: mappings }
-    }
-
-    fn find_output(&self, input: &Range) -> Vec<Range> {
-        let mut outputs: Vec<Range> = vec![];
-        // unused numbers of the original seed range
-        let mut unused: Vec<Range> = vec![*input];
-        for m in &self.mappings {
-            let (output_range, intersected_source_range) = m.map(&input);
-            if output_range != Range::Empty {
-                outputs.push(output_range);
-            }
-            unused = unused.iter().flat_map(|range| range.subtract(&intersected_source_range))
-            .filter(|x| *x != Range::Empty)
-            .collect();
-        }
-
-        if unused.len() > 0 {
-            // the identity mapping, if none of the listed mappings apply
-            outputs.append(&mut unused);
-        }
-
-        return outputs;
-    }
-
-}
-
 
 pub fn solve(input: &str) -> i64 {
+    let time: i64 = input.lines().next().unwrap().split(':').last().unwrap().split_whitespace().collect::<String>().parse().unwrap();
+
+    let distance: i64 = input.lines().last().unwrap().split(':').last().unwrap().split_whitespace().collect::<String>().parse().unwrap();
 
 
-    let separator = format!("{}{}", LINE_ENDING, LINE_ENDING);
-    let another_separator = format!(":{}", LINE_ENDING);
+    let (min, max) = extreme_solutions(&time, &distance);
 
-
-    let mut  mappings = input.split(separator.as_str());
-
-
-    let first_line = mappings.next().expect("Could not read the first (seeds) line");
-    let mut temp = first_line.split(':').last().expect("First row is malformed").trim().split(' ');
-
-    let seed_ranges: Vec<(i64, i64)> = temp.clone()
-        .enumerate()
-        .filter(|(i, _)|  i % 2 == 0)
-        .zip(
-            temp.
-            enumerate()
-            .filter(|(i, _)| i % 2 == 1)
-        )
-        .map(|((i1, e1), (i2, e2))| (e1.parse().expect("could not parse seed initial"), e2.parse().expect("could not parse seed initial")))
-        .collect();
-
-    let seeds: Vec<_> = seed_ranges.iter().map(|(s, w)| Range::Range(*s, *w)).collect();
-
-    let converters: Vec<_> = mappings.clone().map(|mapping_data| {
-        let something = mapping_data.split(another_separator.as_str()).last().expect("Failed to parse mapping entry");
-        // dbg!(something);
-        return Converter::from(something);
-    }).collect();
-
-    let mut current_vals = seeds;
-
-    for conv in converters {
-        current_vals = current_vals.iter().map(|input_range| conv.find_output(input_range)).flatten().collect();
-    }
-
-    return current_vals.iter().fold(i64::MAX, |min, el| {
-        let Range::Range(s, _) = el else {todo!();};
-        if *s < min {
-            return *s;
-        }
-        return min;
-    });
+    return count_solutions((min, max));
 
 }
