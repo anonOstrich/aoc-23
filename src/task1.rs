0bf8f93f48c1e12f730e0547f421b677c89dfc8a
Jesper Kuutti <jesper.kuutti@gmail.com>
2023-12-07 17:57:55 +0200

    7 solved

--- src/task1.rs
+++ src/task1.rs
@@ -1,47 +1,115 @@
+use std::{collections::HashMap, cmp::Ordering};
 
-// For a given T = time_limit we can express the result as a function of charging  time x (0..=T):
-// movement(t) = 0*x + (T-x) * x.
-// Then we're looking for interger solutions to the inequality movement(t) > T
-// by the power of Math we observe that the possible solutions are found between the two zero points of the parabola
-// so we just need to solve the equality movement(t) - T = 0 and we know how many solutions there are
-fn extreme_solutions(time_limit: &i64, record: &i64) -> (f64, f64) {
-    let a = -1 as f64;
-    let b = *time_limit as f64;
-    let c = -1.0 *  (*record as f64);
+#[derive(Debug, PartialEq, Eq, Ord)]
 
-    let mut sol1 = -b + f64::sqrt(b*b - 4.0 * a* c);
-    sol1 /= 2.0 * a;
-
-    let mut sol2 = -b - f64::sqrt(b*b - 4.0 * a* c);
-    sol2 /= 2.0 * a;
-
-    return (sol1, sol2);
+struct Hand {
+    hand_type: HandType,
+    cards: Vec<u8>,
+    bet: u32
 }
 
-fn  is_integer(x: &f64) -> bool {
-    x.fract() == 0.00
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let type_diff: i16 = (self.hand_type as i16) - (other.hand_type as i16);
+        if type_diff > 0 {
+            return Some(Ordering::Greater);
+        } else if type_diff < 0 {
+            return Some(Ordering::Less);
+        }
+
+        for (c1, c2) in self.cards.iter().zip(other.cards.iter()) {
+            if c1 < c2 {
+                return Some(Ordering::Less);
+            }
+            if c1 > c2 {
+                return Some(Ordering::Greater);
+            }
+        }
+        return Some(Ordering::Equal);
+    }
 }
 
-fn count_solutions((min, max): (f64, f64)) -> i64 {
-    let mut narrowing = 0;
-    if is_integer(&min) {
-        narrowing += 1;
+
+#[derive(Debug, PartialEq, Clone, Copy, Eq, PartialOrd, Ord)]
+#[repr(u8)]
+
+enum HandType {
+    FiveOfAKind = 6,
+    FourOfAKind = 5,
+    FullHouse = 4,
+    ThreeOfAKind = 3,
+    TwoPair = 2,
+    OnePair = 1,
+    HighCard = 0
+}
+
+fn card_val_to_number(c: &char) -> u8 {
+    match c {
+        'A' => 14,
+        'K' => 13,
+        'Q' => 12,
+        'J' => 11,
+        'T' => 10,
+        '2'..='9' => c.to_digit(10).unwrap() as u8,
+        _ => panic!("Unknown card value")
     }
-    if is_integer(&max) {
-        narrowing += 1;
+}
+
+impl Hand {
+
+    fn from(hand_str: &str) -> Self{
+        let mut hand_iter = hand_str.split(' ');
+        let cards_str = hand_iter.next().expect("Could not read the hand from input");
+        let bet_str = hand_iter.next().expect("Could not read the bet amount from input");
+
+        let cards = cards_str.chars().map(|c| card_val_to_number(&c)).collect();
+        let bet = bet_str.parse().expect("Could not parse");
+
+
+        let mut map: HashMap<u8, u8> = HashMap::new();
+
+        for card in &cards {
+            if !map.contains_key(card) {
+                map.insert(*card, 0);
+            }
+            map.insert(*card, 
+                *map.get(&card).unwrap() + 1
+            );
+        }
+
+        let counts: Vec<u8> = map.values().map(|x| *x).collect();
+
+        let mut hand_type = HandType::HighCard;
+        if counts.contains(&5){
+             
+         hand_type =  HandType::FiveOfAKind;
+        } else if counts.contains(&4) {
+            hand_type = HandType::FourOfAKind;
+        } else if counts.contains(&3) && counts.contains(&2) {
+            hand_type = HandType::FullHouse;
+        } else if counts.contains(&3) {
+            hand_type =  HandType::ThreeOfAKind;
+        } else if counts.iter().filter(|x| **x == 2).count() == 2 {
+            hand_type =  HandType::TwoPair;
+        } else if counts.contains(&2) {
+            hand_type = HandType::OnePair;
+        } else {
+            hand_type = HandType::HighCard;
+        };
+        
+        return Hand { hand_type: hand_type, cards: cards, bet: bet }
+
+
     }
-    let res = (max.floor() + 1.00) - min.ceil();
-    return res as i64 - narrowing;
 }
 
 pub fn solve(input: &str) -> i64 {
-    let times: Vec<i64> = input.lines().next().unwrap().split(':').last().unwrap().split_whitespace().map(|x| x.parse().unwrap()).collect();
-    let distances: Vec<i64> = input.lines().last().unwrap().split(':').last().unwrap().split_whitespace().map(|x| x.parse().unwrap()).collect();
+    
+    let mut hands: Vec<_> = input.lines().map(|l| Hand::from(l)).collect();
+    hands.sort();
+    
 
-    let solutions: Vec<_> = times.iter().zip(distances.iter()).map(|(t, d)| extreme_solutions(t, d)).collect();
+    let result: usize = hands.iter().enumerate().map(|(idx, hand)| (idx + 1) * (hand.bet as usize)).sum();
 
-    let nof_solutions: Vec<_> = solutions.iter().map(|x| count_solutions(*x)).collect();
-
-
-    return nof_solutions.iter().product();
+    return result as i64;
 }
--- src/task2.rs
+++ src/task2.rs
@@ -1,47 +1,159 @@
+use std::{collections::HashMap, cmp::Ordering};
 
-// For a given T = time_limit we can express the result as a function of charging  time x (0..=T):
-// movement(t) = 0*x + (T-x) * x.
-// Then we're looking for interger solutions to the inequality movement(t) > T
-// by the power of Math we observe that the possible solutions are found between the two zero points of the parabola
-// so we just need to solve the equality movement(t) - T = 0 and we know how many solutions there are
-fn extreme_solutions(time_limit: &i64, record: &i64) -> (f64, f64) {
-    let a = -1 as f64;
-    let b = *time_limit as f64;
-    let c = -1.0 *  (*record as f64);
 
-    let mut sol1 = -b + f64::sqrt(b*b - 4.0 * a* c);
-    sol1 /= 2.0 * a;
+#[derive(Debug, PartialEq, Eq, Ord)]
 
-    let mut sol2 = -b - f64::sqrt(b*b - 4.0 * a* c);
-    sol2 /= 2.0 * a;
-
-    return (sol1, sol2);
+struct Hand {
+    hand_type: HandType,
+    cards: Vec<u8>,
+    bet: u32
 }
 
-fn  is_integer(x: &f64) -> bool {
-    x.fract() == 0.00
-}
+// Doesn't need modifications for jokers
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let type_diff: i16 = (self.hand_type as i16) - (other.hand_type as i16);
+        if type_diff > 0 {
+            return Some(Ordering::Greater);
+        } else if type_diff < 0 {
+            return Some(Ordering::Less);
+        }
 
-fn count_solutions((min, max): (f64, f64)) -> i64 {
-    let mut narrowing = 0;
-    if is_integer(&min) {
-        narrowing += 1;
+        for (c1, c2) in self.cards.iter().zip(other.cards.iter()) {
+            if c1 < c2 {
+                return Some(Ordering::Less);
+            }
+            if c1 > c2 {
+                return Some(Ordering::Greater);
+            }
+        }
+        return Some(Ordering::Equal);
     }
-    if is_integer(&max) {
-        narrowing += 1;
-    }
-    let res = (max.floor() + 1.00) - min.ceil();
-    return res as i64 - narrowing;
 }
+
+#[derive(Debug, PartialEq, Clone, Copy, Eq, PartialOrd, Ord)]
+#[repr(u8)]
+
+enum HandType {
+    FiveOfAKind = 6,
+    FourOfAKind = 5,
+    FullHouse = 4,
+    ThreeOfAKind = 3,
+    TwoPair = 2,
+    OnePair = 1,
+    HighCard = 0
+}
+
+impl HandType {
+    fn from_counts(counts: &Vec<u8>) -> Self {
+        if counts.contains(&5){     
+            return  HandType::FiveOfAKind;
+           } else if counts.contains(&4) {
+               return HandType::FourOfAKind;
+           } else if counts.contains(&3) && counts.contains(&2) {
+               return HandType::FullHouse;
+           } else if counts.contains(&3) {
+               return  HandType::ThreeOfAKind;
+           } else if counts.iter().filter(|x| **x == 2).count() == 2 {
+               return  HandType::TwoPair;
+           } else if counts.contains(&2) {
+               return HandType::OnePair;
+           } else {
+               return HandType::HighCard;
+           };
+    }
+}
+
+fn card_val_to_number(c: &char) -> u8 {
+    match c {
+        'A' => 14,
+        'K' => 13,
+        'Q' => 12,
+        'T' => 10,
+        '2'..='9' => c.to_digit(10).unwrap() as u8,
+        'J' => 1,
+        _ => panic!("Unknown card value")
+    }
+}
+
+impl Hand {
+
+    fn from(hand_str: &str) -> Self{
+        let mut hand_iter = hand_str.split(' ');
+        let cards_str = hand_iter.next().expect("Could not read the hand from input");
+        let bet_str = hand_iter.next().expect("Could not read the bet amount from input");
+
+        let cards: Vec<_> = cards_str.chars().map(|c| card_val_to_number(&c)).collect();
+        let bet = bet_str.parse().expect("Could not parse");
+
+
+        let mut map: HashMap<u8, u8> = HashMap::new();
+
+        for card in &cards {
+            if card == &1 {
+                continue;
+            }
+
+            if !map.contains_key(card) {
+                map.insert(*card, 0);
+            }
+            map.insert(*card, 
+                *map.get(&card).unwrap() + 1
+            );
+        }
+
+        let counts: Vec<u8> = map.iter()
+        // Don't consider jokers at this point -- what is the hand like with only the remaining cards?
+        .filter(|(k, _)| *k != &1).map(|(_, v)| v)
+        .map(|x| *x).collect();
+
+        let mut hand_type = HandType::from_counts(&counts);
+
+        // Best strategy for using jokers is always: 
+        // Convert them all to the value that is most common
+        // If many values could reach the same number of cards,
+        // choose the one with the greatest value
+        if cards.contains(&1) {
+            
+            let nof_jokers: usize = cards.iter().filter(|c| **c == 1).count();
+            let mut replacement: u8 = 0;
+            let mut max_count = 0;
+
+            for (k, v) in &map {
+                if v > &max_count || (v == &max_count && k > &replacement) {
+                    max_count = *v;
+                    replacement = *k;
+                }
+            }
+            
+
+
+            let mut new_map = map.clone();
+            new_map.insert(replacement, max_count + nof_jokers as u8);
+            let new_values: Vec<u8> = new_map.values().map(|x| *x).collect();
+            hand_type = HandType::from_counts(&new_values);
+        }
+
+
+        
+        
+        return Hand { hand_type: hand_type, cards: cards, bet: bet }
+
+
+    }
+}
+
+
+
+
 
 pub fn solve(input: &str) -> i64 {
-    let time: i64 = input.lines().next().unwrap().split(':').last().unwrap().split_whitespace().collect::<String>().parse().unwrap();
+    let mut hands: Vec<_> = input.lines().map(|l| Hand::from(l)).collect();
+    hands.sort();
+    
 
-    let distance: i64 = input.lines().last().unwrap().split(':').last().unwrap().split_whitespace().collect::<String>().parse().unwrap();
+    let result: usize = hands.iter().enumerate().map(|(idx, hand)| (idx + 1) * (hand.bet as usize))
+    .sum();
 
-
-    let (min, max) = extreme_solutions(&time, &distance);
-
-    return count_solutions((min, max));
-
+    return result as i64;
 }
